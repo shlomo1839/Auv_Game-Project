ac08ade658136e5d69eb1ccf35357b8ce846e23b
shlomo1839@gmail.com <shlomoelias1830>
2026-03-20 01:51:44 +0200

    origals

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { Menu } from './pages/Menu';
-import { GameScreen } from './pages/GameScreen';
-import { useGameStore } from './store/GameStore';
-import { GameOverScreen } from './Components/GameOverScreen';
+import { useGameStore } from './store/GameStore.jsx';
+import { Menu } from './pages/Menu.jsx';
+import { GameScreen } from './pages/GameScreen.jsx';
+import { GameOverScreen } from './Components/GameOverScreen.jsx';
 import './App.css'
 
 function App() {
