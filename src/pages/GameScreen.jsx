0201ad703450b287736769e89e3835dd49c23e02
shlomo1839@gmail.com <shlomoelias1830>
2026-03-19 17:04:19 +0200

    added useeffect to create more enemies

--- src/pages/GameScreen.jsx
+++ src/pages/GameScreen.jsx
@@ -1,8 +1,24 @@
 import  {useGameStore } from '../store/GameStore';
 import { Enemy } from '../Components/Enemy';
+import { useState, useEffect } from 'react';
 
 export const GameScreen = () => {
     const { stage, enemiesAlive,  maxEnemies} = useGameStore();
+    const [ enemyList, setEnemyList ] = useState([]);
+    
+    useEffect(() =>{
+        const spawnInterval = setInterval(() => {
+            setEnemyList(prevList => {
+                if (!prevList || prevList.length >= maxEnemies) {
+                    return prevList;
+                }
+                // create uniq id for all enemy by date
+                return [...prevList, Date.now()]
+            })
+        }, 1000)
+        return ()=>clearInterval(spawnInterval)
+    }, [maxEnemies])
+            
 
     return (
         <div className='game-container'>
@@ -11,7 +27,9 @@ export const GameScreen = () => {
                 <span>Alive: {enemiesAlive} / {maxEnemies}</span>
             </div>
             <div className='game-area'>
-                <Enemy />
+                {enemyList.map(id => (
+                    <Enemy key={id} />
+                ))}
             </div>
         </div>
     )
